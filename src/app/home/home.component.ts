d6399872702aafa18136d5307227c7826160f0da
cipherloris <raymarch.perez@gmail.com>
2019-12-03 11:47:58 +0800

    init commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private  firstObsSubscription: Subscription;
+  
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000, ).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create();
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
+
